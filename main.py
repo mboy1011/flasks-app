b53556d0833fc1f5cc8a6396faa1a84a8bceea3f
mboy1011 <lyjieme@gmail.com>
2019-01-31 14:30:59 +0800

    static pages for url_for('static') flask.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import render_template,request,session
+
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return "Welcome to Flask!"
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
