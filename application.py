5e048992609a72aeceb5a1f71528fe3eb798c078
mboy1011 <lyjieme@gmail.com>
2019-01-31 22:43:43 +0800

    Updated...

--- application.py
+++ application.py
@@ -1,12 +1,20 @@
+import os
+
 from flask import Flask
 from flask import render_template,request,session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template('index.html')
+    query = db.execute("SELECT * FROM tbl_employee").fetchall()
+    return render_template('index.html',query=query)
 
 @app.route("/about")
 def about():
